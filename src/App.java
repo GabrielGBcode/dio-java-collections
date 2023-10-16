1fa3ee155e53a6579d8216be9c99feb324bc068a
GabrielGBcode <gabrielgb.xyz@gmail.com>
2023-10-16 13:03:09 -0300

    inital commit

---
+++ src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public class Box<T> {
+        private T t;
+
+        public void set(T t) {
+            this.t = t;
+        }
+
+        public T get() {
+            return t;
+        }
+    }
+}
